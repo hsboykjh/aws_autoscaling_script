628036e42623d90f166a774a55bdca3a029cd357
hsboykjh <hsboykjh@gmail.com>
2016-07-28 18:14:39 +1000

    Update aws_script.sh

--- src/aws_script.sh
+++ src/aws_script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+#old scaling group name
+OLD_AUTO_SCALING_GROUP_NAME=$(aws autoscaling describe-auto-scaling-groups | grep scaling-group | grep api-server-prod | awk '{print $3}')
+echo "OLD_AUTO_SCALING_GROUP_NAME : "$OLD_AUTO_SCALING_GROUP_NAME
+
 # deploy server to make new AMI
 BUILD_INSTANCE=i-9d450f02
 SERVER_SPEC=m3.large
@@ -12,7 +17,7 @@ SERVER_MIN=2
 SERVER_DEFAULT=1
 
 #VPC ID
-VPC_ID=	vpc-4a825d2f
+VPC_ID=vpc-4a825d2f
 SUBNET_ID_1=subnet-5de14d2a
 SUBNET_ID_2=subnet-a5c107fc
 # current Date and Time ( hour/min )
@@ -49,13 +54,31 @@ sleep 180
 AUTO_SCALING_GROUP_NAME=$NAME-scaling-group
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name $AUTO_SCALING_GROUP_NAME --launch-configuration-name $LAUNCH_CONFIGURATION_NAME --load-balancer-names $ELB_NAME --health-check-type ELB --health-check-grace-period 300 --max-size $SERVER_DEFAULT --min-size $SERVER_DEFAULT --desired-capacity $SERVER_DEFAULT --vpc-zone-identifier $SUBNET_ID_1,$SUBNET_ID_2
 
-#'#!/bin/bash
-#cd
-#cp -f /home/ec2-user/api-server/newrelic.yml /home/ec2-user/newrelic/newrelic.yml
-#mkdir ./weldoneTest
-#./start-server.sh'
 
 echo "Create New Auto-Scaling-Group : $AUTO_SCALING_GROUP_NAME is created"
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name sg-0b4b1b6e --launch-configuration-name api-server-prod-201607281139-launch-config --load-balancer-names aim-api-lb --health-check-type EC2 --health-check-grace-period 300 --max-size 1 --min-size 1 --desired-capacity 1 --vpc-zone-identifier subnet-5de14d2a,subnet-a5c107fc
+# TEST create new autoscaling
+#aws autoscaling create-auto-scaling-group --auto-scaling-group-name sg-0b4b1b6e --launch-configuration-name api-server-prod-201607281139-launch-config --load-balancer-names aim-api-lb --health-check-type EC2 --health-check-grace-period 300 --max-size 1 --min-size 1 --desired-capacity 1 --vpc-zone-identifier subnet-5de14d2a,subnet-a5c107fc
+
+# check load balance health check ( instance )
+
+sleep 300
+
+# update autoscaling-group 1=> MAX,  1=> MIN , condition
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name $AUTO_SCALING_GROUP_NAME --min-size $SERVER_MIN --max-size $SERVER_MAX
+echo "$AUTO_SCALING_GROUP_NAME Server MIN: 1 => $SERVER_MIN , MAX 1 => $SERVER_MAX"
+
+sleep 10
+
+# update old version autoscaling-group MAX =>1,  MIN => 1
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name $OLD_AUTO_SCALING_GROUP_NAME --min-size 1 --max-size 1
+echo "$OLD_AUTO_SCALING_GROUP_NAME Server MIN: 1 , MAX 1 "
+
+sleep 300
+
+# deregister old version autoscaling group from load balancer =>  delete old version autoscaling group
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $OLD_AUTO_SCALING_GROUP_NAME
+echo "$OLD_AUTO_SCALING_GROUP_NAME deleted"
+
+aws autoscaling describe-auto-scaling-groups --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId]' --output text | grep ap-northeast-1 | grep running | awk '{print $3}'
 
